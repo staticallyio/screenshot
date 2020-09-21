68860d81ca17d2abdfde019c94de05cdb5a792b4
Frans Allen <git.a@upset.dev>
2020-09-21 08:03:11 +0700

    Control browserWSEndpoint using config file

--- config/index.js
+++ config/index.js
@@ -1,8 +1,11 @@
 'use strict';
 
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    // Public directory containing static files.
-    publicDir: path.join(__dirname, '..', 'public')
+    // Public directory containing static files
+    publicDir: path.join(__dirname, '..', 'public'),
+
+    // Remote browser endpoint
+    browserWSEndpoint: process.env.REMOTE_BROWSER || 'ws://localhost:3000'
 }
