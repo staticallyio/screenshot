8c4335a04fab898f6f0614eee99af616f1d40da3
Frans Allen <git.a@upset.dev>
2021-01-10 00:05:02 +0700

    change environment variable name for port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/screenshot/pdf/*', getPdf);
 app.get('/screenshot/mobile/*', getMobile);
 app.get('/*', getDesktop);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT_SCREENSHOT || 5000;
 app.listen(port, () =>
   console.log(`Screenshot listen on http://localhost:${port}`),
 );
