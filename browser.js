2df47351835681dc8ddd9f9119bae1ee3a51ebf8
Frans Allen <fransallen@users.noreply.github.com>
2021-10-14 19:13:42 +0700

    removed referer header

--- browser.js
+++ browser.js
@@ -12,9 +12,6 @@ async function getScreenshot(url, type, quality, fullPage) {
   });
   const page = await browser.newPage();
 
-  await page.setExtraHTTPHeaders({
-    referer: 'https://statically.io/screenshot/',
-  });
   await page.emulate({
     userAgent:
       'Mozilla/5.0 (Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Statically-Screenshot/1.0 (+https://statically.io/screenshot/)',
@@ -46,9 +43,6 @@ async function getScreenshotMobile(url, type, quality, fullPage) {
   });
   const page = await browser.newPage();
 
-  await page.setExtraHTTPHeaders({
-    referer: 'https://statically.io/screenshot/',
-  });
   await page.emulate({
     userAgent:
       'Mozilla/5.0 (Linux Android 5.0 SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Mobile Safari/537.36 Statically-Screenshot-Mobile/1.0 (+https://statically.io/screenshot/)',
@@ -80,9 +74,6 @@ async function generatePdf(url) {
   });
   const page = await browser.newPage();
 
-  await page.setExtraHTTPHeaders({
-    referer: 'https://statically.io/screenshot/',
-  });
   await page.emulate({
     userAgent:
       'Mozilla/5.0 (Linux Android 5.0 SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Mobile Safari/537.36 Statically-Screenshot-PDF/1.0 (+https://statically.io/screenshot/)',
